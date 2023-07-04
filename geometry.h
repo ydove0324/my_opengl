581ebf9f8bb5b9698755eed7531e4b62aef8d688
y_dove <1352035939@qq.com>
2023-07-04 16:47:05 +0800

    3D render

--- geometry.h
+++ geometry.h
@@ -16,6 +16,9 @@ template <class t> struct Vec2 {
 	inline Vec2<t> operator +(const Vec2<t> &V) const { return Vec2<t>(u+V.u, v+V.v); }
 	inline Vec2<t> operator -(const Vec2<t> &V) const { return Vec2<t>(u-V.u, v-V.v); }
 	inline Vec2<t> operator *(float f)          const { return Vec2<t>(u*f, v*f); }
+	// Vec2<t>& operator [](int id) {
+	// 	if (id == 0)	
+	// }
 	template <class > friend std::ostream& operator<<(std::ostream& s, Vec2<t>& v);
 };
 
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "model.h"
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <limits>
 const TGAColor white = TGAColor{255, 255, 255, 255};
 const TGAColor red   = TGAColor{0, 0,   255,   255};
 void line(int x0,int y0,int x1,int y1,TGAImage &image, TGAColor color) {
@@ -54,13 +56,10 @@ Vec3f barycentric(Vec2i p0,Vec2i* tr) {
     if (_temp.x == 0)   return Vec3f(-1,1,1);
     return Vec3f(1. - (_temp.y + _temp.z) / _temp.x,_temp.y / _temp.x,_temp.z / _temp.x);
 }
-void triangle (Vec2i*tr,TGAImage &image,TGAColor color) {
-    // std::vector<Vec2i>tr;
-    // tr.push_back(p0);
-    // tr.push_back(p1);
-    // tr.push_back(p2);
-    // Vec2i tr[3] = {p0,p1,p2};
-    Vec2i border_min(image.width() - 1,image.height() - 1);
+void triangle (Vec3i*tr,float *z_buffer,TGAImage &image,TGAColor color) {
+    int width = image.width() - 1;
+    int height = image.height() - 1;
+    Vec2i border_min(width,height);
     Vec2i border_max(0,0);
     for (int i = 0; i < 3; ++i) {
         border_max.x = std::max(border_max.x,tr[i].x);
@@ -68,56 +67,61 @@ void triangle (Vec2i*tr,TGAImage &image,TGAColor color) {
         border_min.x = std::min(border_min.x,tr[i].x);
         border_min.y = std::min(border_min.y,tr[i].y);
     }
-    border_max.x = std::min(border_max.x,image.width() - 1);
-    border_max.y = std::min(border_max.y,image.height() - 1);
+    border_max.x = std::min(border_max.x,width);
+    border_max.y = std::min(border_max.y,height);
+    Vec2i D2_tr[3];
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            D2_tr[i].raw[j] = tr[i].raw[j];
+        }
+    }
+    auto ID = [=](int x,int y) -> int {
+        return x * width + y;
+    };
     for (int x = border_min.x; x <= border_max.x; ++x) {
         for (int y = border_min.y; y <= border_max.y; ++y) {
-            Vec3f bc_screen = barycentric(Vec2i(x,y),tr);
+            Vec3f bc_screen = barycentric(Vec2i(x,y),D2_tr);
             if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0)  continue;
-            // std::cerr << x << ' ' << y << '\n';
-            image.set(x,y,color);
+            float H = 0;
+            for (int j = 0; j < 3; ++j)     H += bc_screen.z * tr[j].z;
+            if (H > z_buffer[ID(x,y)]) {
+                z_buffer[ID(x,y)] = H;
+                image.set(x,y,color);
+            }
+            
         }
     }
 }
 void Render(TGAImage &image) {
-    int width = image.width();
-    int height = image.height();
+    int width = image.width() - 1;
+    int height = image.height() - 1;
     Model model("D:\\note\\my_openGL\\my_opengl\\obj\\african_head\\african_head.obj");
-    // for (int i=0; i< model.nfaces(); i++) { 
-    //     std::vector<int> face = model.face(i); 
-    //     for (int j=0; j<3; j++) { 
-    //         Vec3f v0 = model.vert(face[j]); 
-    //         Vec3f v1 = model.vert(face[(j+1)%3]); 
-    //         int x0 = (v0.x+1.)*width/2.; 
-    //         int y0 = (v0.y+1.)*height/2.; 
-    //         int x1 = (v1.x+1.)*width/2.; 
-    //         int y1 = (v1.y+1.)*height/2.; 
-    //         line(x0, y0, x1, y1, image, white); 
-    //     } 
-    // }
-    // TGAImage frame(200, 200, TGAImage::RGB); 
     Vec3f light_dir = Vec3f(0,0,-1);
+    float *z_buffer = new float [width * height];
+    std::fill(z_buffer,z_buffer+(width*height),-std::numeric_limits<float>::max());
+    std::cerr << z_buffer[0] << '\n';
     for (int i=0; i<model.nfaces(); i++) { 
         std::vector<int> face = model.face(i); 
-        Vec2i screen_coords[3]; 
+        Vec3i screen_coords[3]; 
         Vec3f _temp[3];
         for (int j=0; j<3; j++) { 
             Vec3f world_coords = model.vert(face[j]); 
-            screen_coords[j] = Vec2i((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.); 
+            screen_coords[j] = Vec3i((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.,(world_coords.z)); 
             _temp[j] = world_coords;
         } 
         Vec3f n = (_temp[2] - _temp[0]) ^ (_temp[1] - _temp[0]);
         n.normalize();
         auto light_intensity = n * light_dir;
         if (light_intensity > 0) {
-            Vec2i tr[3] = {screen_coords[0], screen_coords[1], screen_coords[2]};
-            triangle(tr, image, TGAColor{(uint8_t)(255*light_intensity), (uint8_t)(255*light_intensity), (uint8_t)(255*light_intensity), 255}); 
+            Vec3i tr[3] = {screen_coords[0], screen_coords[1], screen_coords[2]};
+            triangle(tr, z_buffer,image, TGAColor{(uint8_t)(255*light_intensity), (uint8_t)(255*light_intensity), (uint8_t)(255*light_intensity), 255}); 
         }
         // delete []_temp;
     }  
+    delete []z_buffer;
 }
-const int WIDTH = 512;
-const int HEIGHT = 512;
+const int WIDTH = 1024;
+const int HEIGHT = 1024;
 int main(int argc, char** argv) {
     TGAImage image(WIDTH,HEIGHT, TGAImage::RGB);
     // image.set(52, 41, red);
