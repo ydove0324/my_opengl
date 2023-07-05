ac799288c4dd45bd9602489f5c856336e3f853e1
y_dove <1352035939@qq.com>
2023-07-05 10:24:01 +0800

    update the Model

--- model.cpp
+++ model.cpp
@@ -21,16 +21,27 @@ Model::Model(const char *filename) : verts_(), faces_() {
             verts_.push_back(v);
         } else if (!line.compare(0, 2, "f ")) {
             std::vector<int> f;
-            int itrash, idx;
+            std::vector<int> t;
+            int itrash, face_idx;
+            int text_idx;
             iss >> trash;
-            while (iss >> idx >> trash >> itrash >> trash >> itrash) {
-                idx--; // in wavefront obj all indices start at 1, not zero
-                f.push_back(idx);
+            while (iss >> face_idx >> trash >> text_idx >> trash >> itrash) {
+                face_idx--; // in wavefront obj all indices start at 1, not zero
+                text_idx--;
+                t.push_back(text_idx);
+                f.push_back(face_idx);
             }
             faces_.push_back(f);
+        } else if (!line.compare(0,3,"vt ")) {
+            Vec2f v;
+            float ftrash;
+            for (int i = 0; i < 2; ++i)     iss >> v.raw[i];
+            iss >> ftrash;
+            texts_.push_back(v);
         }
     }
-    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
+    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() 
+    << "# vt#" << texts_.size() << std::endl;
 }
 
 Model::~Model() {
@@ -44,10 +55,23 @@ int Model::nfaces() {
     return (int)faces_.size();
 }
 
+int Model::ntext() {
+    return (int)texts_.size();
+}
+
 std::vector<int> Model::face(int idx) {
     return faces_[idx];
 }
 
+std::vector<int> Model::face_text(int idx) {
+    return face_texts_[idx];
+}
+
+
 Vec3f Model::vert(int i) {
     return verts_[i];
+}
+
+Vec2f Model::text(int i) { //text[i]:texture coordinate
+    return texts_[i];
 }
--- model.h
+++ model.h
@@ -8,13 +8,18 @@ class Model {
 private:
 	std::vector<Vec3f> verts_;
 	std::vector<std::vector<int> > faces_;
+	std::vector<Vec2f> texts_;
+	std::vector<std::vector<int>> face_texts_;
 public:
 	Model(const char *filename);
 	~Model();
 	int nverts();
+	int ntext();
 	int nfaces();
 	Vec3f vert(int i);
+	Vec2f text(int i);//texture coordinate
 	std::vector<int> face(int idx);
+	std::vector<int> face_text(int idx);
 };
 
 #endif //__MODEL_H__
